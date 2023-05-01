dbefed32d4343847709e958be079790fa777a6e6
CrownedJester <kiyoshisatoshy@gmail.com>
2023-05-01 21:57:35 +0300

    Combine attributes with their colors

--- app/src/main/java/com/crownedjester/soft/pokedexapp/data/local/entity/Stats.kt
+++ app/src/main/java/com/crownedjester/soft/pokedexapp/data/local/entity/Stats.kt
@@ -1,5 +1,12 @@
 package com.crownedjester.soft.pokedexapp.data.local.entity
 
+import com.crownedjester.soft.pokedexapp.presentation.ui.theme.AtkColor
+import com.crownedjester.soft.pokedexapp.presentation.ui.theme.DefColor
+import com.crownedjester.soft.pokedexapp.presentation.ui.theme.HPColor
+import com.crownedjester.soft.pokedexapp.presentation.ui.theme.SpAtkColor
+import com.crownedjester.soft.pokedexapp.presentation.ui.theme.SpDefColor
+import com.crownedjester.soft.pokedexapp.presentation.ui.theme.SpdColor
+
 data class Stats(
     val stats: List<Stat>
 )
@@ -8,6 +15,19 @@ data class Stat(
     val baseStat: Int,
     val effort: Int,
     val name: String
-)
+) {
+    companion object {
+
+        val statsWithColors = mapOf(
+            "hp" to HPColor,
+            "attack" to AtkColor,
+            "defense" to DefColor,
+            "special-attack" to SpAtkColor,
+            "special-defense" to SpDefColor,
+            "speed" to SpdColor
+        )
+
+    }
+}
 
 
